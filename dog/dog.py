6b9840562488fbb10e94e7297b6b96523602d0bc
Thomas Mercurio <thomas_mercurio@brown.edu>
2016-08-10 22:58:29 -0700

    more dog

--- dog/dog.py
+++ dog/dog.py
@@ -8,12 +8,12 @@ class Dog:
 
     def __init__(self, bot):
         self.bot = bot
-        self.url = 'http://random.dog/woof/'
+        self.url = 'http://random.dog/'
 
     @commands.command(pass_context=True, no_pm=True)
     async def dog(self, ctx):
         """Shows a random dog."""
-        async with aiohttp.get(self.url) as response:
+        async with aiohttp.get(self.url + 'woof') as response:
             await self.bot.say(self.url + await response.text())
 
 def setup(bot):
