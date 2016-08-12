850eabd1ddeae54297b175c724e40fe6fb4c0cd8
Thomas Mercurio <thomas_mercurio@brown.edu>
2016-08-11 18:48:09 -0700

    leaving should work?

--- quakesounds/quakesounds.py
+++ quakesounds/quakesounds.py
@@ -18,6 +18,15 @@ class Quakesounds:
     def voice_client(self, server):
         return self.bot.voice_client_in(server)
 
+    async def leave_channel(self, context):
+        server = context.message.server
+        if not self.voice_connected(server):
+            return
+        voice_client = self.voice_client(server)
+        if self.audio_player:
+            self.audio_player.stop()
+        await voice_client.disconnect()
+
     @commands.group(pass_context=True, no_pm=True, name='voice', aliases=['vc'])
     async def _vc(self, context):
         """[join/leave]"""
@@ -56,266 +65,266 @@ class Quakesounds:
         gender = 'female'
         sound = 'bottomfeeder'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fdominating')
     async def _fdominating(self, context):
         gender = 'female'
         sound = 'dominating'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='ffirstblood')
     async def _ffirstblood(self, context):
         gender = 'female'
         sound = 'firstblood'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fgodlike')
     async def _fgodlike(self, context):
         gender = 'female'
         sound = 'godlike'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fheadshot')
     async def _fheadshot(self, context):
         gender = 'female'
         sound = 'headshot'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fholyshit')
     async def _fholyshit(self, context):
         gender = 'female'
         sound = 'holyshit'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fkillingspree')
     async def _fkillingspree(self, context):
         gender = 'female'
         sound = 'killingspree'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fmonsterkill')
     async def _fmonsterkill(self, context):
         gender = 'female'
         sound = 'monsterkill'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fmultikill')
     async def _fmultikill(self, context):
         gender = 'female'
         sound = 'multikill'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fprepare')
     async def _fprepare(self, context):
         gender = 'female'
         sound = 'prepare'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='frampage')
     async def _frampage(self, context):
         gender = 'female'
         sound = 'rampage'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fultrakill')
     async def _fultrakill(self, context):
         gender = 'female'
         sound = 'ultrakill'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='funstoppable')
     async def _funstoppable(self, context):
         gender = 'female'
         sound = 'unstoppable'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='fwickedsick')
     async def _fwickedsick(self, context):
         gender = 'female'
         sound = 'wickedsick'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='combobreaker')
     async def _combobreaker(self, context):
         gender = 'male'
         sound = 'combobreaker'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='combowhore')
     async def _combowhore(self, context):
         gender = 'male'
         sound = 'combowhore'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='dominating')
     async def _dominating(self, context):
         gender = 'male'
         sound = 'dominating'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='doublekill')
     async def _doublekill(self, context):
         gender = 'male'
         sound = 'doublekill'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='firstblood')
     async def _firstblood(self, context):
         gender = 'male'
         sound = 'firstblood'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='godlike')
     async def _godlike(self, context):
         gender = 'male'
         sound = 'godlike'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='hattrick')
     async def _hattrick(self, context):
         gender = 'male'
         sound = 'hattrick'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='headhunter')
     async def _headhunter(self, context):
         gender = 'male'
         sound = 'headhunter'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='headshot')
     async def _headshot(self, context):
         gender = 'male'
         sound = 'headshot'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='holyshit')
     async def _holyshit(self, context):
         gender = 'male'
         sound = 'holyshit'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='humiliation')
     async def _humiliation(self, context):
         gender = 'male'
         sound = 'humiliation'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='impressive')
     async def _impressive(self, context):
         gender = 'male'
         sound = 'impressive'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='killingspree')
     async def _killingspree(self, context):
         gender = 'male'
         sound = 'killingspree'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='ludicrouskill')
     async def _ludicrouskill(self, context):
         gender = 'male'
         sound = 'ludicrouskill'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='monsterkill')
     async def _monsterkill(self, context):
         gender = 'male'
         sound = 'monsterkill'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='multikill')
     async def _multikill(self, context):
         gender = 'male'
         sound = 'multikill'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='perfect')
     async def _perfect(self, context):
         gender = 'male'
         sound = 'perfect'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='prepare')
     async def _prepare(self, context):
         gender = 'male'
         sound = 'prepare'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='rampage')
     async def _rampage(self, context):
         gender = 'male'
         sound = 'rampage'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='teamkiller')
     async def _teamkiller(self, context):
         gender = 'male'
         sound = 'teamkiller'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='triplekill')
     async def _triplekill(self, context):
         gender = 'male'
         sound = 'triplekill'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='ultrakill')
     async def _ultrakill(self, context):
         gender = 'male'
         sound = 'ultrakill'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='unstoppable')
     async def _unstoppable(self, context):
         gender = 'male'
         sound = 'unstoppable'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     @commands.command(no_pm=True, pass_context=True, name='wickedsick')
     async def _wickedsick(self, context):
         gender = 'male'
         sound = 'wickedsick'
         await self.sound_play(context, gender, sound)
-        await self._leave(context)
+        await self.leave_channel(context)
 
     async def sound_init(self, server, path):
         voice_client = self.voice_client(server)
