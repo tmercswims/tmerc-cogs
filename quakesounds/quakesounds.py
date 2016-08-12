d5e42f05116dbdadbcb7502179e8cdbb71cba260
Thomas Mercurio <thomas_mercurio@brown.edu>
2016-08-12 00:17:21 -0700

    quakesounds now work (fuck async sometimes)

--- quakesounds/quakesounds.py
+++ quakesounds/quakesounds.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 from __main__ import send_cmd_help
 from .utils import checks
 from os.path import join
+from time import sleep
+import threading
 
 class Quakesounds:
     """Play some Quake sounds."""
@@ -11,21 +13,60 @@ class Quakesounds:
         self.sound_base = 'data/downloader/tmerc-cogs/quakesounds/data/sounds'
 
     def voice_connected(self, server):
-        if self.bot.is_voice_connected(server):
-            return True
-        return False
+        return self.bot.is_voice_connected(server)
 
     def voice_client(self, server):
         return self.bot.voice_client_in(server)
 
-    def leave_channel(self, context):
+    async def _join_voice_channel(self, context):
+        channel = context.message.author.voice_channel
+        if channel:
+            await self.bot.join_voice_channel(channel)
+
+    async def _leave_voice_channel(self, context):
         server = context.message.server
         if not self.voice_connected(server):
             return
         voice_client = self.voice_client(server)
+
         if self.audio_player:
             self.audio_player.stop()
-        voice_client.disconnect()
+        await voice_client.disconnect()
+
+    async def sound_init(self, context, path):
+        server = context.message.server
+        options = '-filter "volume=volume=0.25"'
+        voice_client = self.voice_client(server)
+        self.audio_player = voice_client.create_ffmpeg_player(path, options=options)
+
+    async def sound_play(self, context, gender, sound):
+        path = join(self.sound_base, gender, sound + '.mp3')
+        server = context.message.server
+        if not context.message.author.voice_channel:
+            await self.bot.say("```You need to join a voice channel first!```")
+            return
+        if not context.message.channel.is_private:
+            if self.voice_connected(server):
+                if not self.audio_player:
+                    await self.sound_init(context, path)
+                    threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
+                else:
+                    if not self.audio_player.is_playing():
+                        await self.sound_init(context, path)
+                        threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
+            else:
+                await self._join_voice_channel(context)
+                if not self.audio_player:
+                    await self.sound_init(context, path)
+                    threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
+                else:
+                    if not self.audio_player.is_playing():
+                        await self.sound_init(context, path)
+                        threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
+
+    def sound_thread(self, t, context):
+        t.run()
+        self.voice_client(context.message.server).loop.create_task(self._leave_voice_channel(context))
 
     @commands.group(pass_context=True, no_pm=True, name='voice', aliases=['vc'])
     async def _vc(self, context):
@@ -240,6 +281,12 @@ class Quakesounds:
         sound = 'multikill'
         await self.sound_play(context, gender, sound)
 
+    @commands.command(no_pm=True, pass_context=True, name='ownage')
+    async def _ownage(self, context):
+        gender = 'male'
+        sound = 'ownage'
+        await self.sound_play(context, gender, sound)
+
     @commands.command(no_pm=True, pass_context=True, name='perfect')
     async def _perfect(self, context):
         gender = 'male'
@@ -287,22 +334,6 @@ class Quakesounds:
         gender = 'male'
         sound = 'wickedsick'
         await self.sound_play(context, gender, sound)
-
-    async def sound_init(self, server, path):
-        voice_client = self.voice_client(server)
-        self.audio_player = voice_client.create_ffmpeg_player(path)
-
-    async def sound_play(self, context, gender, sound):
-        path = join(self.sound_base, gender, sound + '.mp3')
-        server = context.message.server
-        if not context.message.channel.is_private:
-            if self.voice_connected(server) and not self.audio_player:
-                await self.sound_init(server, path)
-                self.audio_player.start()
-            elif self.audio_player:
-                if not self.audio_player.is_playing():
-                    await self.sound_init(server, path)
-                    self.audio_player.start()
 
 def setup(bot):
     bot.add_cog(Quakesounds(bot))
