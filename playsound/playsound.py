03b2622957169198e132885101b001884954d3dd
Thomas Mercurio <thomas.mercurio@jitterbit.com>
2016-08-12 22:31:32 +0100

    added playsound and voice; quakesounds to be removed

---
+++ playsound/playsound.py
@@ -0,0 +1,105 @@
+from discord.ext import commands
+from __main__ import send_cmd_help
+from .utils import checks
+
+import glob
+import os
+import os.path
+import threading
+
+class Playsound:
+    """Play a sound byte."""
+    def __init__(self, bot):
+        self.bot = bot
+        self.audio_player = False
+        self.sound_base = 'data/playsound'
+
+    def list_sounds(self):
+        return [os.path.splitext(s)[0] for s in os.listdir(self.sound_base)]
+
+    def voice_connected(self, server):
+        return self.bot.is_voice_connected(server)
+
+    def voice_client(self, server):
+        return self.bot.voice_client_in(server)
+
+    async def _join_voice_channel(self, context):
+        channel = context.message.author.voice_channel
+        if channel:
+            await self.bot.join_voice_channel(channel)
+
+    async def _leave_voice_channel(self, context):
+        server = context.message.server
+        if not self.voice_connected(server):
+            return
+        voice_client = self.voice_client(server)
+
+        if self.audio_player:
+            self.audio_player.stop()
+        await voice_client.disconnect()
+
+    async def sound_init(self, context, path):
+        server = context.message.server
+        options = '-filter "volume=volume=0.25"'
+        voice_client = self.voice_client(server)
+        self.audio_player = voice_client.create_ffmpeg_player(path, options=options)
+
+    async def sound_play(self, context, p):
+        server = context.message.server
+        if not context.message.author.voice_channel:
+            await self.bot.say("```You need to join a voice channel first!```")
+            return
+        if not context.message.channel.is_private:
+            if self.voice_connected(server):
+                if not self.audio_player:
+                    await self.sound_init(context, p)
+                    threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
+                else:
+                    if not self.audio_player.is_playing():
+                        await self.sound_init(context, p)
+                        threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
+            else:
+                await self._join_voice_channel(context)
+                if not self.audio_player:
+                    await self.sound_init(context, p)
+                    threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
+                else:
+                    if not self.audio_player.is_playing():
+                        await self.sound_init(context, p)
+                        threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
+
+    def sound_thread(self, t, context):
+        t.run()
+        self.voice_client(context.message.server).loop.create_task(self._leave_voice_channel(context))
+
+    @commands.command(no_pm=True, pass_context=True, name='playsound')
+    async def _playsound(self, context, soundname):
+        """Plays the specified sound."""
+        f = glob.glob(os.path.join(self.sound_base, soundname + '.*'))
+        if len(f) < 1:
+            await self.bot.say('```Sound file not found! Try !allsounds.```')
+            return
+        elif len(f) > 1:
+            await self.bot.say("""```You have {} sound files with the same name, but different extensions, and I can't deal with it.
+                                     Please make filenames (excluding extensions) unique!```""".format(len(f)))
+            return
+
+        p = os.path.join(self.sound_base, f[0])
+        await self.sound_play(context, p)
+
+    @commands.command(pass_context=True, name='allsounds')
+    async def _allsounds(self, context):
+        """Sends a list of every sound in a PM."""
+        strbuffer = self.list_sounds()
+        mess = ""
+        for line in strbuffer:
+            if len(mess) + len(line) + 1 < 2000:
+                mess += "\n" + line
+            else:
+                await self.bot.send_message(context.message.author, mess)
+                mess = ""
+        if mess != "":
+            await self.bot.send_message(context.message.author, mess)
+
+def setup(bot):
+    bot.add_cog(Playsound(bot))
---
+++ voice/voice.py
@@ -0,0 +1,51 @@
+from discord.ext import commands
+from __main__ import send_cmd_help
+from .utils import checks
+
+class Voice:
+    """Tools for controlling the bot's voice connections."""
+    def __init__(self, bot):
+        self.bot = bot
+        self.audio_player = False
+
+    def voice_connected(self, server):
+        return self.bot.is_voice_connected(server)
+
+    def voice_client(self, server):
+        return self.bot.voice_client_in(server)
+
+    @commands.group(pass_context=True, no_pm=True, name='voice', aliases=['vc'])
+    async def _voice(self, context):
+        """[join/leave]"""
+        if context.invoked_subcommand is None:
+            await send_cmd_help(context)
+
+    @_vc.command(hidden=True, pass_context=True, no_pm=True, name='join', aliases=['connect'])
+    @checks.serverowner_or_permissions()
+    async def _join(self, context):
+        """Joins your voice channel."""
+        author = context.message.author
+        server = context.message.server
+        channel = author.voice_channel
+        if not self.voice_connected(server):
+            await self.bot.join_voice_channel(channel)
+
+    @_vc.command(hidden=True, pass_context=True, no_pm=True, name='leave', aliases=['disconnect'])
+    @checks.serverowner_or_permissions()
+    async def _leave(self, context):
+        """Leaves your voice channel."""
+        server = context.message.server
+        if not self.voice_connected(server):
+            return
+        voice_client = self.voice_client(server)
+        if self.audio_player:
+            self.audio_player.stop()
+        await voice_client.disconnect()
+
+    @_vc.command(no_pm=True, name='stop')
+    async def _stop(self):
+        if self.audio_player:
+            self.audio_player.stop()
+
+def setup(bot):
+    bot.add_cog(Voice(bot))
