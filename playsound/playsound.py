cd219908802dd0d8fbfbf4527a13c8952b411fc3
Thomas Mercurio <thomas.mercurio@jitterbit.com>
2016-08-16 22:57:57 +0100

    addsound complete

--- playsound/playsound.py
+++ playsound/playsound.py
@@ -123,7 +123,7 @@ class Playsound:
 
         filepath = os.path.join(self.sound_base, filename)
 
-        if os.path.isfile(filepath):
+        if filename in self.list_sounds():
             await self.bot.say("```A sound with that filename already exists!```")
             return
 
