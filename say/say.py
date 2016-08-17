916868c854a4d0a598d64c42cd6e2a4a5d2be536
Thomas Mercurio <thomas.mercurio@jitterbit.com>
2016-08-17 17:45:50 +0100

    added tts to Say

--- say/say.py
+++ say/say.py
@@ -10,7 +10,6 @@ class Say:
     @commands.command(no_pm=True, pass_context=True)
     async def say(self, ctx, *, text):
         """Repeats what you tell it.
-        Can use `message`, `channel`, `server`
         """
         user = ctx.message.author
         if hasattr(user, 'bot') and user.bot is True:
@@ -26,6 +25,24 @@ class Say:
         if len(str(evald)) > 2000:
             evald = str(evald)[-1990:] + " you fuck."
         await self.bot.say(evald)
+
+    @commands.command(no_pm=True, pass_context=True)
+    async def tts(self, ctx, *, text):
+        """TTS's what you tell it."""
+        user = ctx.message.author
+        if hasattr(user, 'bot') and user.bot is True:
+            return
+        try:
+            if "__" in text:
+                raise ValueError
+            evald = eval(text, {}, {'message': ctx.message,
+                                    'channel': ctx.message.channel,
+                                    'server': ctx.message.server})
+        except:
+            evald = text
+        if len(str(evald)) > 2000:
+            evald = str(evald)[-1990:] + " you fuck."
+        await self.bot.send_message(context.message.channel, evald, tts=True)
 
 def setup(bot):
     bot.add_cog(Say(bot))
