bd3cb5cd70a6e1862304ae4e73ea101d63e5e4d2
Thomas Mercurio <tmercswims@gmail.com>
2016-08-27 20:10:04 -0700

    indentation

--- playsound/playsound.py
+++ playsound/playsound.py
@@ -59,8 +59,8 @@ class Playsound:
                     threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
                 else:
                     if not self.audio_player.is_playing():
-                    await self.sound_init(context, p)
-                    threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
+                        await self.sound_init(context, p)
+                        threading.Thread(target=self.sound_thread, args=(self.audio_player, context,)).start()
             else:
                 await self._join_voice_channel(context)
                 if not self.audio_player:
