d2d89336837b1de47d5bae279de8a1719ab5afe8
Thomas Mercurio <tmercswims@gmail.com>
2016-10-17 19:05:40 -0700

    [Catfact] Updated to get an arbitrary number of facts.
    (Within reason.)

--- catfact/catfact.py
+++ catfact/catfact.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from .utils import chat_formatting as cf
 
 import aiohttp
 import json
@@ -11,16 +12,28 @@ class Catfact:
 
     def __init__(self, bot: commands.bot.Bot):
         self.bot = bot
-        self.url = "https://catfacts-api.appspot.com/api/facts?number=1"
+        self.url = "https://catfacts-api.appspot.com/api/facts?number={}"
 
     @commands.command(pass_context=True, no_pm=True, name="catfact")
-    async def _catfact(self, ctx: commands.context.Context):
-        """Gets a random cat fact."""
+    async def _catfact(self, ctx: commands.context.Context, number: str="1"):
+        """Gets random cat facts."""
 
         await self.bot.type()
-        async with aiohttp.get(self.url) as response:
-            fact = json.loads(await response.text())["facts"][0]
-            await self.bot.say(fact)
+
+        n = 0
+        try:
+            n = int(number)
+        except ValueError:
+            await self.bot.reply(cf.error("That is not a number!"))
+            return
+
+        if n > 10:
+            await self.bot.reply(cf.warning("Be reasonable, please."))
+            return
+
+        async with aiohttp.get(self.url.format(number)) as response:
+            for fact in json.loads(await response.text())["facts"]:
+                await self.bot.say(fact)
 
 
 def setup(bot: commands.bot.Bot):
