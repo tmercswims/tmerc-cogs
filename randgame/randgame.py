b225b9b7cf6636bb3f4969672a79e47646b10140
Thomas Mercurio <thomas.mercurio@jitterbit.com>
2016-11-10 23:08:40 +0000

    [RandGame] Fixed timeout condition for question.

--- randgame/randgame.py
+++ randgame/randgame.py
@@ -151,7 +151,7 @@ class RandGame:
         answer = await self.bot.wait_for_message(timeout=15,
                                                  author=ctx.message.author)
 
-        if answer is not None and answer.content.lower().strip() != "yes":
+        if answer is None or answer.content.lower().strip() != "yes":
             await self.bot.reply("Game list not replaced.")
             return
 
