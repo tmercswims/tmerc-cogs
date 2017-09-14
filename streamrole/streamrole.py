4838e873a3379b7f55418bd45960976286bfea7c
Matti <me@mattisony.no>
2017-09-14 03:35:26 +0200

    [StreamRole] Added option to only promote from a specific role
    Also fixed member not showing on forbidden error

--- streamrole/streamrole.py
+++ streamrole/streamrole.py
@@ -12,7 +12,8 @@ from .utils import checks, chat_formatting as cf
 
 default_settings = {
     "enabled": False,
-    "role": None
+    "role": None,
+    "only": None
 }
 
 
@@ -33,6 +34,9 @@ class StreamRole:
         server = ctx.message.server
         if server.id not in self.settings:
             self.settings[server.id] = deepcopy(default_settings)
+            dataIO.save_json(self.settings_path, self.settings)
+        if "only" not in self.settings[server.id]:
+            self.settings[server.id]["only"] = None
             dataIO.save_json(self.settings_path, self.settings)
         if ctx.invoked_subcommand is None:
             await self.bot.send_cmd_help(ctx)
@@ -62,6 +66,28 @@ class StreamRole:
                 cf.info("StreamRole is now disabled."))
         dataIO.save_json(self.settings_path, self.settings)
 
+    @_streamroleset.command(pass_context=True, no_pm=True, name="only")
+    @checks.admin_or_permissions(manage_server=True)
+    async def _only(self, ctx: commands.Context, role: discord.Role):
+        """Sets the role from which to update streamers.
+
+        Only members with this role will be given the streaming
+        role when streaming. Set to '@everyone' to reset it."""
+
+        await self.bot.type()
+
+        server = ctx.message.server
+        if role.id == server.id:
+            self.settings[server.id]["only"] = None
+        else:
+            self.settings[server.id]["only"] = role.id
+        dataIO.save_json(self.settings_path, self.settings)
+
+        await self.bot.reply(
+            cf.info("Only members with the role `{}` will be given the "
+                    "streaming role now. Set it to `@everyone` to "
+                    "reset it.".format(role.name)))
+
     @_streamroleset.command(pass_context=True, no_pm=True, name="role")
     @checks.admin_or_permissions(manage_server=True)
     async def _role(self, ctx: commands.Context, role: discord.Role):
@@ -85,22 +111,41 @@ class StreamRole:
         if before.server.id not in self.settings:
             self.settings[before.server.id] = deepcopy(default_settings)
             dataIO.save_json(self.settings_path, self.settings)
+        elif "only" not in self.settings[before.server.id]:
+            self.settings[before.server.id]["only"] = None
+            dataIO.save_json(self.settings_path, self.settings)
 
         server_settings = self.settings[before.server.id]
         if server_settings["enabled"] and server_settings["role"] is not None:
             streamer_role = find(lambda m: m.id == server_settings["role"],
                                  before.server.roles)
+            only_role = find(lambda l: l.id == server_settings["only"],
+                             before.server.roles)
             if streamer_role is None:
                 return
+
             # is streaming
             if (after.game is not None and
                     after.game.type == 1 and
                     streamer_role not in after.roles):
-                await self.bot.add_roles(after, streamer_role)
+                if (only_role is None or only_role in after.roles):
+                    try:
+                        await self.bot.add_roles(after, streamer_role)
+                    except discord.Forbidden:
+                        print("StreamRole: forbidden error\n"
+                              "Server: {}, Role: {}, Member: {}".format(
+                                  before.server.id, streamer_role.id, after.id)
+                              )
+
             # is not
             elif ((after.game is None or after.game.type != 1) and
                   streamer_role in after.roles):
-                await self.bot.remove_roles(after, streamer_role)
+                try:
+                    await self.bot.remove_roles(after, streamer_role)
+                except discord.Forbidden:
+                    print("StreamRole: forbidden error\n"
+                          "Server: {}, Role: {}, Member: {}".format(
+                              before.server.id, streamer_role.id, after.id))
 
 
 def check_folders():
