bbbea719c8828b3a0fc1541dc43fd3d2262d8439
Thomas Mercurio <tmercswims@gmail.com>
2017-12-07 20:22:20 +0000

    [CatFact] Initial conversion.

---
+++ catfact/__init__.py
@@ -0,0 +1,4 @@
+from .catfact import CatFact
+
+def setup(bot):
+  bot.add_cog(CatFact())
---
+++ catfact/catfact.py
@@ -0,0 +1,32 @@
+import logging
+
+import aiohttp
+from discord.ext import commands
+
+log = logging.getLogger('red.tmerc.catfact')
+
+
+class CatFact:
+  '''Gets a random cat fact.'''
+
+  def __init__(self):
+    self.__url = 'https://catfact.ninja/fact'
+    self.__session = aiohttp.ClientSession()
+
+  def __unload(self):
+    if self.__session:
+      self.__session.close()
+
+  @commands.command()
+  async def catfact(self, ctx: commands.Context):
+    '''Gets a random cat fact.'''
+
+    await ctx.trigger_typing()
+
+    try:
+      async with self.__session.get(self.__url) as response:
+        fact = (await response.json())['fact']
+        await ctx.send(fact)
+    except:
+      log.warning('API call failed; unable to get cat fact')
+      await ctx.send('I was unable to get a cat fact.')
