b62ab75c94b6c84e780c12fd283f95cb843b88cd
Thomas Mercurio <tmercswims@gmail.com>
2017-12-07 22:54:43 +0000

    [MassDM] Initial conversion.

---
+++ massdm/__init__.py
@@ -0,0 +1,5 @@
+from .massdm import MassDM
+
+
+def setup(bot):
+  bot.add_cog(MassDM())
---
+++ massdm/massdm.py
@@ -0,0 +1,50 @@
+import logging
+from typing import List
+
+import discord
+from discord.ext import commands
+
+from redbot.core import checks
+
+log = logging.getLogger('red.tmerc.massdm')
+
+
+class MassDM:
+  '''Send a direct message to all members of the specified Role.'''
+
+  def __init__(self):
+    pass
+
+  @commands.command(aliases=['mdm'])
+  @commands.guild_only()
+  @checks.mod_or_permissions(manage_server=True)
+  async def massdm(self, ctx: commands.Context, role: discord.Role, *, message: str):
+    '''Sends a DM to all Members with the given Role.
+    Allows for the following customizations:
+    {0} is the member being messaged
+    {1} is the role through which they are being messaged
+    {2} is the server through which they are being messaged
+    {3} is you, the person sending the message
+    '''
+
+    try:
+      await ctx.message.delete()
+    except discord.Forbidden:
+      log.warning('Failed to delete command message: insufficient permissions')
+    except:
+      log.warning('Failed to delete command message')
+
+    dm_these = self._get_members_with_role(ctx.guild, role)
+
+    for member in dm_these:
+      try:
+        await member.send(message.format(member, role, ctx.guild, ctx.author))
+      except discord.Forbidden:
+        log.warning('Failed to DM user {0} (ID {0.id}): insufficient permissions'.format(member))
+        continue
+      except:
+        log.warning('Failed to DM user {0} (ID {0.id})'.format(member))
+        continue
+
+  def _get_members_with_role(self, guild: discord.Guild, role: discord.Role) -> List[discord.Member]:
+    return [m for m in guild.members if role in m.roles]
